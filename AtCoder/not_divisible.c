c4efa0794621a303bc33d96c55cc0f2b2a0d7b0f
vaijayanthi-01 <66869524+vaijayanthi-01@users.noreply.github.com>
2020-06-18 08:39:12 +0530

    Update not_divisible.c

--- AtCoder/not_divisible.c
+++ AtCoder/not_divisible.c
@@ -1,1 +1,8 @@
+/* Q: Given is a number sequence A of length N.
+Find the number of integers i (1 <= i <= N) with the following property:
+  For every integer j (1 <= j <= N) such that i != j, Aj doesn't divide Ai. */
 
+/* i/p: 5
+        24 11 8 3 16
+   o/p: 3
+   The integers with the property are 2, 3 and 4. */
