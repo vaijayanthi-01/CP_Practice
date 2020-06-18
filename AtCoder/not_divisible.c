72607a76d511142d202aefc93072faa1d0216e75
vaijayanthi-01 <66869524+vaijayanthi-01@users.noreply.github.com>
2020-06-18 11:33:43 +0530

    Update not_divisible.c

--- AtCoder/not_divisible.c
+++ AtCoder/not_divisible.c
@@ -6,3 +6,41 @@ Find the number of integers i (1 <= i <= N) with the following property:
         24 11 8 3 16
    o/p: 3
    The integers with the property are 2, 3 and 4. */
+
+//Sieve of Eratosthenes
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int comfun(const void* a, const void *b){
+  return (*(int*)a - *(int*)b);
+}
+
+int main(void){
+  int N, i;
+  scanf("%d", &N);
+  int A[N];
+  
+  for(i = 0; i < N; ++i)
+     scanf("%d", &A[i]);
+  
+  qsort(A, N, sizeof(int), comfun);
+  
+  int arr[1000001] = {0}, ans = 0;
+  
+  for(i = 0; i < N; ++i){
+    
+    if(arr[A[i]] == 0){
+      ++ans;
+      for(int j = 1; j * A[i] <= 1000000; ++j)
+        arr[j * A[i]] = 1;
+      if(i < N - 1){
+        if(A[i] == A[i + 1])
+          --ans;
+      }
+    }
+    
+  }
+  printf("%d\n", ans);
+  return 0;
+}
