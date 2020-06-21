4b627f445e0fc110ff6702aa84f4e43e74237c1b
vaijayanthi-01 <66869524+vaijayanthi-01@users.noreply.github.com>
2020-06-21 16:22:48 +0530

    Create C+=.cpp

---
+++ CodeForces/C+=.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ 
+int main(void)
+{
+    int t;
+    cin >> t;
+    int count = 0;
+    while(t--)
+    {
+        int a, b, n;
+        cin >> a >> b >>n;
+        int count = 0;
+        while(a <= n && b <= n)
+        {
+            if(a < b)
+            {
+                a += b;
+                ++count;
+            }
+            else
+            {
+                b += a;
+                ++count;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
