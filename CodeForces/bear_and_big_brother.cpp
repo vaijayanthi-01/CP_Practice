0b9d673acfcea69692beb322a1bdf28f4113f306
vaijayanthi-01 <66869524+vaijayanthi-01@users.noreply.github.com>
2020-06-29 17:14:24 +0530

    Create bear_and_big_brother.cpp

---
+++ CodeForces/bear_and_big_brother.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/791/A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    
+    int years = 0;
+    while(a <= b)
+    {
+        a *= 3;
+        b *= 2;
+        ++years;
+    }
+    
+    cout << years << endl;
+    return 0;
+}
